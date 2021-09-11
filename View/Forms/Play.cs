c2109dcd1653c4c0d74dacead9d8ab254b8402f7
PABLO EMILIO VILLA PEREZ <pablovilla266477@correo.itm.edu.co>
2021-09-11 00:17:52 -0500

    Form Play created

---
+++ View/Forms/Play.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace View.Forms
+{
+    public partial class Play : Form
+    {
+        public Play()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            Questions questions = new Questions();
+            questions.Show();
+            this.Visible = false;
+        }
+    }
+}
